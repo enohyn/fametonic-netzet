751d7a6fc0b96b948e4b12fa01d72e6d2621d6c1
shayok-viva <shayok.ullah@vivasoftdltd.com>
2025-07-05 01:10:54 +0600

    Hero image optimized

--- components/phone-mockup.tsx
+++ components/phone-mockup.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function PhoneMockup() {
   return (
     <section className="h-full w-full">
-      <Image src="/images/fametonic-mobile-art.png" alt="Phone Mockup" width={666} height={679} className="absolute -top-8 left-[495px] hidden lg:block" />
+      <Image src="/images/fametonic-mobile-art.png" alt="Phone Mockup" width={666} height={679} className="absolute -top-8 left-[495px] hidden lg:block left-optimized" />
       <div className="flex justify-center lg:hidden">
         <Image src="/images/fametonic-mobile-art.png" alt="Phone Mockup" width={390} height={426} objectFit="cover" />
       </div>
